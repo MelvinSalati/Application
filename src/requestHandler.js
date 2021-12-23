ae032788bed2fca8685a9764c10366814ba8f7b8
MelvinSalati <Melvinsalati@gmail.com>
2021-12-23 06:09:08 +0200

    changed_url_to_localhost

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -1,6 +1,8 @@
 import Axios from "axios";
 
 const request = Axios.create({
-  baseURL: "https://v2.smart-umodzi.com:443/",
+  baseURL:
+    // "https://v2.smart-umodzi.com:443/"
+    "http://127.0.0.1:8001",
 });
 export default request;
