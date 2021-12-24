1039b550e68895eda2262abde681d19eae1bdc4c
MelvinSalati <Melvinsalati@gmail.com>
2021-12-24 16:32:50 +0200

    25_12_2021

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -601,65 +601,54 @@ const Search = () => {
     [babyID, clientUuid]
   );
 
-  useEffect(
-    (clientUuid) => {
-      if (clientUuid) {
-        // baby appointments
-        async function getEmtct() {
-          const request = await axios.get(`/api/v1/client/baby/${clientUuid}`);
-          if (request.data.status === 200) {
-            setBabies(request.data.show);
-          } else {
-            setBabies([]);
-          }
-        }
-        getEmtct();
-        // clients appointments
-        async function clientAppointments() {
-          const request = await axios.get(
-            `/api/v1/client/appointments/${clientUuid}`
-          );
-          setAppointments(request.data.appointments);
-        }
-        // tracking
-        async function trackingHistory() {
-          const request = await axios.get(
-            `/api/v1/client/tracking/${clientUuid}`
-          );
-          setMissedAppointments(request.data.appointments);
-        }
-        // cleint status
-        async function getStatus() {
-          const request = await axios.get(
-            `/api/v1/client/status/${clientUuid}`
-          );
-          setStatus(request.data.status);
-          setStatusCode(request.data.code);
-        }
-        // contact list status
-        async function getContacts() {
-          const request = await axios.get(
-            `/api/v1/client/contacts/${clientUuid}`
-          );
-          setViewContacts(request.data.contacts);
-        }
-        async function getDeath() {
-          const request = await axios.get(`/api/v1/client/death/${clientUuid}`);
-          setViewDeaths(request.data.death);
-        }
-        //
-        getDeath();
-        getContacts();
-
-        clientAppointments();
-        // tracking History
-        trackingHistory();
-        // get status informatio
-        getStatus();
+  useEffect(() => {
+    // baby appointments
+    async function getEmtct() {
+      const request = await axios.get(`/api/v1/client/baby/${clientUuid}`);
+      if (request.data.status === 200) {
+        setBabies(request.data.show);
+      } else {
+        setBabies([]);
       }
-    },
-    [clientUuid]
-  );
+    }
+    getEmtct();
+    // clients appointments
+    async function clientAppointments() {
+      const request = await axios.get(
+        `/api/v1/client/appointments/${clientUuid}`
+      );
+      setAppointments(request.data.appointments);
+    }
+    // tracking
+    async function trackingHistory() {
+      const request = await axios.get(`/api/v1/client/tracking/${clientUuid}`);
+      setMissedAppointments(request.data.appointments);
+    }
+    // client  status
+    async function getStatus() {
+      const request = await axios.get(`/api/v1/client/status/${clientUuid}`);
+      setStatus(request.data.status);
+      setStatusCode(request.data.code);
+    }
+    // contact list status
+    async function getContacts() {
+      const request = await axios.get(`/api/v1/client/contacts/${clientUuid}`);
+      setViewContacts(request.data.contacts);
+    }
+    async function getDeath() {
+      const request = await axios.get(`/api/v1/client/death/${clientUuid}`);
+      setViewDeaths(request.data.death);
+    }
+    //
+    getDeath();
+    getContacts();
+
+    clientAppointments();
+    // tracking History
+    trackingHistory();
+    // get status informatio
+    getStatus();
+  }, [clientUuid]);
 
   // Transfer
   useEffect(() => {
@@ -945,7 +934,9 @@ const Search = () => {
                   >
                     <path d="M8 14.933a.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067v13.866zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
                   </svg>
-                  <span className="text-success">{status}</span>
+                  <span className="text-success">
+                    <strong>{status}</strong>
+                  </span>
                 </>
               ) : (
                 <></>
@@ -963,7 +954,9 @@ const Search = () => {
                   >
                     <path d="M8 14.933a.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067v13.866zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
                   </svg>{" "}
-                  <span className="text-warning">{status}</span>
+                  <span className="text-warning">
+                    <strong>{status}</strong>
+                  </span>
                 </>
               ) : (
                 <></>
@@ -980,7 +973,9 @@ const Search = () => {
                   >
                     <path d="M8 14.933a.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067v13.866zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
                   </svg>{" "}
-                  <span className="text-danger">{status}</span>
+                  <span className="text-danger">
+                    <strong>{status}</strong>
+                  </span>
                 </>
               ) : (
                 <></>
--- src/requestHandler.js
+++ src/requestHandler.js
@@ -4,5 +4,6 @@ const request = Axios.create({
   baseURL:
     // "https://v2.smart-umodzi.com:443/"
     "https://api.v1.smart-umodzi.com:443/public/",
+  // "http://127.0.0.1:8001/",
 });
 export default request;
