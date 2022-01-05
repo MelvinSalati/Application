faae07f9ca06e87a3932fbd26bb0400434706b36
MelvinSalati <Melvinsalati@gmail.com>
2022-01-05 16:48:58 +0200

    last

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 const request = Axios.create({
   baseURL:
     // "https://v2.smart-umodzi.com:443/"
-    // "https://api.v1.smart-umodzi.com:443/public/",
-    "http://127.0.0.1:8002/",
+    "https://api.v1.smart-umodzi.com:443/public/",
+  // "http://127.0.0.1:8002/",
 });
 export default request;
