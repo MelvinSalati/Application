2bb22b84f53293a9d61c49489f07d7ef59739cfd
MelvinSalati <Melvinsalati@gmail.com>
2023-08-01 21:45:20 +0200

    changed serhukve2r2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4acebe6a6c2c77f3e00dca40804abeaa.js"
+  "./precache-manifest.74f984f0d5fb2a90fbdc342b258c232d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
