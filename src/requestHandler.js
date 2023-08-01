a96816fe12005d2203f7ba70c0251089ab29dea5
MelvinSalati <Melvinsalati@gmail.com>
2023-08-01 22:14:49 +0200

    changed serh09ukvdce2r2

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const request = Axios.create({
-  baseURL: "http://95.216.225.167/~umodzixy/api.v1/public/",
+  baseURL: "https://95.216.225.167/~umodzixy/api.v1/public/",
   // baseURL: "https://api.train.smart-umodzi.com:443/public/",
   // baseURL: "http://127.0.0.1:8000/",
 });
