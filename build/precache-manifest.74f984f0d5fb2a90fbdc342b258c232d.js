68a2914501e9abc6536ce957604d1f0e799f2f00
MelvinSalati <Melvinsalati@gmail.com>
2023-08-01 21:58:14 +0200

    changed serh09ukve2r2

---
+++ build/precache-manifest.74f984f0d5fb2a90fbdc342b258c232d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c88bca9ba734f33eab3",
+    "url": "./static/css/main.114c3aed.chunk.css"
+  },
+  {
+    "revision": "4c88bca9ba734f33eab3",
+    "url": "./static/js/main.4c88bca9.chunk.js"
+  },
+  {
+    "revision": "501c93b5f01289d55fb0",
+    "url": "./static/js/1.501c93b5.chunk.js"
+  },
+  {
+    "revision": "2c8386aa6c9293a2b97c",
+    "url": "./static/css/2.8b63015d.chunk.css"
+  },
+  {
+    "revision": "2c8386aa6c9293a2b97c",
+    "url": "./static/js/2.2c8386aa.chunk.js"
+  },
+  {
+    "revision": "bce31713c825cb949eea",
+    "url": "./static/js/runtime~main.bce31713.js"
+  },
+  {
+    "revision": "8ea47e091bac21ed7d77a9447b5f5933",
+    "url": "./static/media/process.8ea47e09.gif"
+  },
+  {
+    "revision": "602886f74026efd7444fabec7392c956",
+    "url": "./index.html"
+  }
+];
